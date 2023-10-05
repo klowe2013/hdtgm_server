163324b24a355ef26702e5eb6903b58e25bd137c
Kaleb Lowe <kalebalowe91@gmail.com>
2023-10-05 18:19:51 -0400

    Basics are working without GCP integration

--- blueprints/get_by_id/routes.py
+++ blueprints/get_by_id/routes.py
@@ -1,18 +1,13 @@
 from flask import Blueprint, current_app, Response, send_file
-from python.constants import FILE_PATH_TABLE, EPISODE_INFO, GCLOUD_PREFIX
+from python.constants import FILE_PATH_TABLE, EPISODE_INFO
 import json 
 import time 
 import base64 
 from io import BytesIO
-from google.cloud import storage
-
-client = storage.Client('hdtgm-player')
-bucket = client.get_bucket('hdtgm-episodes')
 
 app = current_app
 with app.app_context():
     database = current_app.config['database']
-    r = current_app.config['redis']
     id_bp = Blueprint(
         'id_bp', __name__, static_folder='static'
     )
@@ -62,8 +57,7 @@ def audio_stream(id):
 
     print(f'Streaming "{this_file}" from GCP')
     def generate():
-        audio_blob = bucket.blob(this_file)
-        with audio_blob.open('rb') as f:
+        with open(this_file, 'rb') as f:
             data = f.read(1024)
             n_reads = 0
             while data:
@@ -78,35 +72,19 @@ def get_audio_by_id(id):
     print(f'pulling audio for id {id}')
     # Default to pull from Redis cache
     start_time = time.time()
-    if hasattr(r, 'set'):
-        # Get audio data, or return None if not in redis
-        audio_data = r.get(f'audio_data:{id}')
-    else:
-        # Default to None for next conditional check
-        audio_data = None 
+    
+    # Get path to file
+    this_file = database.query(
+        f"""
+        select FILEPATH from {FILE_PATH_TABLE} 
+        where id=='{id}'
+        """
+    )['FILEPATH'].values[0]
 
-    if audio_data is None:
-        # Get path to file
-        this_file = database.query(
-            f"""
-            select FILEPATH from {FILE_PATH_TABLE} 
-            where id=='{id}'
-            """
-        )['FILEPATH'].values[0]
-
-        # with open(this_file, 'rb') as f:
-        #     audio_data = base64.b64encode(f.read()).decode('UTF-8')
-        #     print(f'Loaded data from source file in {time.time()-start_time:.2f}s: {audio_data[:100]}')
-        #     if hasattr(r, 'set'): r.set(f'audio_data:{id}', audio_data)
-        print(f'Reading "{this_file}" from GCP')
-        audio_blob = bucket.blob(this_file)
-        with audio_blob.open('rb') as f:
-            audio_data = base64.b64encode(f.read()).decode('UTF-8')
-            print(f'Loaded data from source file in {time.time()-start_time:.2f}s')
-            if hasattr(r, 'set'): r.set(f'audio_data:{id}', audio_data)
-    else:
-        print(f'Loaded data from cache in {time.time()-start_time:.2f}s')
-
+    with open(this_file, 'rb') as f:
+        audio_data = base64.b64encode(f.read()).decode('UTF-8')
+        print(f'Loaded data from source file in {time.time()-start_time:.2f}s: {audio_data[:100]}')
+        
     data = {"snd": audio_data}
     res = app.response_class(response=json.dumps(data),
         status=200,
--- blueprints/upload_download/routes.py
+++ blueprints/upload_download/routes.py
@@ -1,13 +1,9 @@
 from flask import Blueprint, request, current_app
 from werkzeug.utils import secure_filename
-from python.constants import FILE_PATH_TABLE, SQLITE_FILEPATH_SCHEMA , GCLOUD_PREFIX
+from python.constants import FILE_PATH_TABLE, SQLITE_FILEPATH_SCHEMA
 from python.EpisodeIngestor import EpisodeIngestor 
 import uuid 
 import os 
-from google.cloud import storage
-
-client = storage.Client('hdtgm-player')
-bucket = client.get_bucket('hdtgm-episodes')
 
 app = current_app
 with app.app_context():
@@ -23,24 +19,14 @@ def episode_upload():
     upload_folder = './media/audio_files'
     uploaded_files = request.files.getlist('file')
     for uploaded_file in uploaded_files:
-        filename = secure_filename(uploaded_file.filename)
-        uploaded_file.save(os.path.join(upload_folder, filename))
+        uploaded_file.save(os.path.join(upload_folder, uploaded_file.filename))
 
         # Get title, episode number, ID
         internal_id = str(uuid.uuid4())
-        # Open blob on GCP
-        print('Opening blob')
-        blob = bucket.blob(os.path.join(GCLOUD_PREFIX, uploaded_file.filename))
-        # Upload the file
-        print(f'Uploading {filename} to blob')
-        blob.upload_from_filename(os.path.join(upload_folder, filename))
-        # Remove from local
-        print('cleaning locally')
-        os.remove(os.path.join(upload_folder, filename))
         
         id_info = {
             'id': internal_id,
-            'filepath': os.path.join(GCLOUD_PREFIX, uploaded_file.filename)
+            'filepath': os.path.join(upload_folder, uploaded_file.filename)
         }
 
         print(f'Entering path info {id_info} to DB')
