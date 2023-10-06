397ade6f3f36a42fef74d4cf4d13cab78bbbfb22
Kaleb Lowe <kalebalowe91@gmail.com>
2023-10-06 08:38:59 -0400

    Removed Cassandra interface for minimal running version

--- python/db_interfaces/DatabaseFactory.py
+++ python/db_interfaces/DatabaseFactory.py
@@ -1,9 +1,7 @@
-from python.db_interfaces.CassandraInterface import CassandraInterface
 from python.db_interfaces.SQLiteInterface import SQLiteInterface
 
 class DatabaseFactory:
     INTERFACE_MAP = {
-        'cassandra': CassandraInterface,
         'sqlite': SQLiteInterface,
     }
     def __init__(self, database):
